84ad3486de8b5be60bcd87ed8c52f616baf59302
tsousacorreia <tsousacorreia@outlook.com>
2024-09-03 22:13:41 -0300

    Atualizado em 03-09-24

--- app/src/main/java/br/com/etec/lancheira/MainActivity.java
+++ app/src/main/java/br/com/etec/lancheira/MainActivity.java
@@ -2,23 +2,23 @@ package br.com.etec.lancheira;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ConstrutoresFragment(), "Construtores");
+        adapter.addFragment(new ReguladoresFragment(), "Reguladores");
+        adapter.addFragment(new EnergeticosFragment(), "Energéticos");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
